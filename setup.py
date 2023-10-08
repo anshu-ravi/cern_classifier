8deee567b682ed304de349827a3cc0f50cac8faf
anshu <r.anshumaan01@gmail.com>
2023-10-08 14:17:16 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to build a ML model to predict whether a collision from the test dataset generates bijets, trijets or tetrajets. It also includes fairness and interpretability analysis along with MLOps best practices.',
+    author='Anshu',
+    license='MIT',
+)
