60c31e46e997e92ba6c3a2e966bc20e8b65294cd
anshu <r.anshumaan01@gmail.com>
2023-10-29 18:56:14 +0100

    Update logging method and added FE driver func

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,30 +1,15 @@
-import logging
 from typing import Union, List
 from pickle import dump, load
 import json
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
-import yaml
+from loggers.log_factory import setup_logging
 
-
-try:
-    with open("config.yaml", "r", encoding="utf-8") as config_file:
-        config_data = yaml.load(config_file, Loader=yaml.FullLoader)
-        logging_rules = config_data.get("logging", {})
-        logging.basicConfig(
-            level=logging.getLevelName(config_data["logging"]["level"]),
-            format=config_data["logging"]["format"],
-        )
-
-except FileNotFoundError:
-    print(f"Error: File path not found.")
-except yaml.YAMLError as e:
-    print(f"Error parsing YAML: {e}")
+logging = setup_logging(__name__)
 
 
 def create_buckets(
-    data: pd.DataFrame, cols: List[str], buckets: Union[None, List[float]] = None
-):
+    data: pd.DataFrame, cols: List[str], test: bool = False):
     """
     Discretizes the values in the specified columns of the input DataFrame into
     four equal-frequency buckets (quartiles) using pandas.qcut, or into the
@@ -33,44 +18,46 @@ def create_buckets(
     Args:
         data (pandas.DataFrame): The input DataFrame.
         cols (list of str): The names of the columns to discretize.
-        buckets (list or ndarray, optional): The edges of the bins to use for
-            discretization, as in pandas.cut. For train data, this should be
-            None as the bins will be computed and for test data,
-            this should be passed as the bins computed for the train data.
+        test (bool, optional): Whether the function is being called on train
+            data or test data. Defaults to False.
 
     Returns:
         pandas.DataFrame: The input DataFrame with the specified columns
         discretized into quartiles or the specified buckets.
-        List[float]: The bins computed for the train data.
     """
     try:
-        if buckets is None:
+        if not test:
             logging.info("Creating buckets for train data.")
+            bb_bucket = None
             for col in cols:
-                data[col], buckets = pd.qcut(
+                data[col], bb_bucket = pd.qcut(
                     data[col],
                     q=4,
                     labels=["Q1", "Q2", "Q3", "Q4"],
                     retbins=True,
                     precision=0,
                 )
-            return data, list(buckets) if buckets is not None else []
-
-        for col in cols:
-            logging.info("Assigning buckets for test")
-            data[col] = pd.cut(
-                data[col],
-                bins=buckets,
-                labels=["Q1", "Q2", "Q3", "Q4"],
-                include_lowest=True,
-            )
-        return data
+            if bb_bucket is not None:
+                with open("./features/buckets.json", "w") as f:
+                    json.dump(list(bb_bucket), f)
+            return data
+        else:
+            with open("./features/buckets.json", "r") as f:
+                buckets = json.load(f)
+            for col in cols:
+                logging.info("Assigning buckets for test")
+                data[col] = pd.cut(
+                    data[col],
+                    bins=buckets,
+                    labels=["Q1", "Q2", "Q3", "Q4"],
+                    include_lowest=True)
+            return data
     except Exception as e:
         logging.error(f"Error occurred while creating buckets: {e}")
         raise
 
 
-def convert_to_categorical(data: pd.DataFrame, cols: List[str], train: bool = True):
+def convert_to_categorical(data: pd.DataFrame, cols: List[str], test: bool):
     """
     Converts the specified columns of the input DataFrame into one-hot encoded
     categorical variables using One-hot encoding.
@@ -86,14 +73,14 @@ def convert_to_categorical(data: pd.DataFrame, cols: List[str], train: bool = Tr
         converted to one-hot encoded categorical variables.
     """
     try:
-        if train:
+        if not test:
             logging.info("Converting columns to categorical for train data.")
             ohe_encoder = OneHotEncoder(sparse_output=False, drop="first")
             encoded_data = ohe_encoder.fit_transform(data[cols])
-            dump(ohe_encoder, open("notebooks/encoder.pkl", "wb"))
+            dump(ohe_encoder, open("./features/encoder.pkl", "wb"))
         else:
             logging.info("Converting columns to categorical for test data.")
-            ohe_encoder = load(open("notebooks/encoder.pkl", "rb"))
+            ohe_encoder = load(open("./features/encoder.pkl", "rb"))
             encoded_data = ohe_encoder.transform(data[cols])
 
         column_names = ohe_encoder.get_feature_names_out(input_features=cols)
@@ -107,7 +94,7 @@ def convert_to_categorical(data: pd.DataFrame, cols: List[str], train: bool = Tr
         raise
 
 
-def factorize(df: pd.DataFrame, test: bool = False) -> pd.DataFrame:
+def factorize(df: pd.DataFrame, test: bool) -> pd.DataFrame:
     """
     Factorizes the 'jets' column of the input DataFrame and saves the definitions
     to a JSON file if test is False. If test is True, the definitions are loaded
@@ -127,11 +114,11 @@ def factorize(df: pd.DataFrame, test: bool = False) -> pd.DataFrame:
             df["jets"] = factor[0]
             logging.info('Successfully factorized "jets" column.')
             defs = factor[1]
-            with open("./defs.json", "w", encoding="utf-8") as json_file:
+            with open("./features/defs.json", "w", encoding="utf-8") as json_file:
                 json.dump(defs.tolist(), json_file)
             return df
         else:
-            with open("./defs.json", encoding="utf-8") as json_file:
+            with open("./features/defs.json", encoding="utf-8") as json_file:
                 defs = json.load(json_file)
             df["jets"] = pd.Categorical(df["jets"], categories=defs)
             df["jets"] = df["jets"].cat.codes
@@ -140,4 +127,36 @@ def factorize(df: pd.DataFrame, test: bool = False) -> pd.DataFrame:
 
     except Exception as e:
         logging.error(f"Error occurred while factorizing 'jets' column: {e}")
+        raise
+
+def run_fe(df: pd.DataFrame,
+    bucket_cols: List[str],
+    categorical_cols: List[str],
+    test: bool = False):
+    """
+    Runs all feature engineering functions on the input DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame.
+        bucket_cols (list of str): The names of the columns to discretize.
+        buckets (list or ndarray): The edges of the bins to use for
+            discretization, as in pandas.cut.
+        categorical_cols (list of str): The names of the columns to convert
+            to categorical variables.
+        test (bool, optional): Whether the function is being called on train
+            data or test data. Defaults to False.
+
+    Returns:
+        pandas.DataFrame: The input DataFrame with all feature engineering
+        functions applied.
+    """
+    try:
+        logging.info("Running feature engineering.")
+        df = create_buckets(df, bucket_cols)
+        df = convert_to_categorical(df, categorical_cols, test)
+        df = factorize(df, test)
+        logging.info("Feature engineering complete.")
+        return df
+    except Exception as e:
+        logging.error(f"Error occurred while running feature engineering: {e}")
         raise
