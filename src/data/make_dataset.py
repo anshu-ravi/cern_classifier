a83c46e34262e6e32dc01039c1efac9b45bac03c
anshu <r.anshumaan01@gmail.com>
2023-10-29 18:52:13 +0100

    Update logging method & remove main function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,34 +4,12 @@ import sys
 from pathlib import Path
 from typing import List
 import pandas as pd
-import yaml
 import click
 from dotenv import find_dotenv, load_dotenv
+from loader.data_loader import data_loader
+from loggers.log_factory import setup_logging
 
-sys.path.append(
-    str(
-        Path(
-            "/mnt/c/Users/ransh/Documents/IE University/Year 5/Sem1/MLOps/cern_classifer/"
-        ).resolve()
-    )
-)
-from src.loader.data_loader import data_loader
-
-
-try:
-    with open("config.yaml", "r", encoding="utf-8") as config_file:
-        config_data = yaml.load(config_file, Loader=yaml.FullLoader)
-        logging_rules = config_data.get("logging", {})
-        logging.basicConfig(
-            level=logging.getLevelName(config_data["logging"]["level"]),
-            format=config_data["logging"]["format"],
-        )
-
-except FileNotFoundError:
-    print(f"Error: File path not found.")
-except yaml.YAMLError as e:
-    print(f"Error parsing YAML: {e}")
-
+logger = setup_logging(__name__)
 
 def merge_data(df1: pd.DataFrame, df2: pd.DataFrame, column_name: str) -> pd.DataFrame:
     """
@@ -46,13 +24,13 @@ def merge_data(df1: pd.DataFrame, df2: pd.DataFrame, column_name: str) -> pd.Dat
         pandas.DataFrame: The merged dataframe.
     """
     try:
-        logging.info(f"Merging data on {column_name}")
+        logger.info(f"Merging data on {column_name}")
         data = pd.merge(df1, df2, on=column_name)
-        logging.info(f"Successfully merged data on {column_name}")
+        logger.info(f"Successfully merged data on {column_name}")
         return data
 
     except Exception as e:
-        logging.error(f"Error merging data: {e}")
+        logger.error(f"Error merging data: {e}")
         raise
 
 
@@ -74,30 +52,11 @@ def load_data(input_filepath: List[str], output_filepath: str) -> None:
         df1 = data_loader(input_filepath[0])
         df2 = data_loader(input_filepath[1])
 
+        logger.info("Converting raw data into interim data")
         data = merge_data(df1, df2, "id")
-        data.to_csv(output_filepath)
-        logging.info(f"Successfully saved merged data to {output_filepath}")
+        data.to_csv(output_filepath, index=False)
+        logger.info(f"Successfully saved merged data to {output_filepath}")
 
     except Exception as e:
-        logging.error(f"Error loading or merging data: {e}")
+        logger.error(f"Error loading or merging data: {e}")
         raise
-
-
-def main():
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info("Converting raw data into interim data")
-    load_data()
-
-
-if __name__ == "__main__":
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
